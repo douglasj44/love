3e709bfe73fce7b829e1542a5a8f70f72cd3afd3
Douglas José Martins <68973272+douglasj44@users.noreply.github.com>
2024-08-19 23:18:10 -0400

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+var btn = document.querySelector(".no");
+var position = 0;
+var isAnimating = false;
+
+btn.addEventListener("click", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+btn.addEventListener("mouseover", function() {
+    if (!isAnimating) {
+        isAnimating = true;
+        position = position === 0 ? 150 : 0;
+        btn.style.transform = `translate(0px, ${position}px)`;
+        btn.style.transition = "all 0.2s ease";
+
+        setTimeout(function() {
+            isAnimating = false;
+        }, 200);
+    }
+});
+
+const sim = document.getElementById('yes');
+
+sim.addEventListener("click", () => {
+
+let timerInterval;
+Swal.fire({
+  title: 'Obrigado 😍',
+  html: 'Prometo lhe fazer feliz. 💘',
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: () => {
+    Swal.showLoading();
+    const b = Swal.getHtmlContainer().querySelector('b');
+    timerInterval = setInterval(() => {
+      b.textContent = Swal.getTimerLeft();
+    }, 100);
+  },
+  willClose: () => {
+    clearInterval(timerInterval);
+  }
+}).then((result) => {
+  if (result.dismiss === Swal.DismissReason.timer) {
+    // Abrir o vídeo do YouTube após o timer terminar
+    window.location.href = "https://www.youtube.com/watch?v=fMmfd4L3LMo";
+  }
+});
+
+});
